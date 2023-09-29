5f323b4ed4f035b6d7783be5219ba2e34c56925e
solidsnakedev <jona.ca.eng@gmail.com>
2023-09-28 20:08:56 -0400

    add preprod

---
+++ preprod/cardano-scripts/run-cardano-node.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+HOSTADDR="0.0.0.0"
+PORT="6000"
+TOPOLOGY="/node/configuration/topology.json"
+CONFIG="/node/configuration/config.json"
+DBPATH="/node/db"
+SOCKETPATH="/node/ipc/node.socket"
+KESKEY="/node/pool-keys/kes.skey"
+VRFKEY="/node/pool-keys/vrf.skey"
+NODECERT="/node/pool-keys/node.cert"
+    
+/usr/local/bin/cardano-node run \
+        --topology ${TOPOLOGY} \
+        --database-path ${DBPATH} \
+        --socket-path ${SOCKETPATH} \
+        --host-addr ${HOSTADDR} \
+        --port ${PORT} \
+        --config ${CONFIG} \
+        --shelley-kes-key ${KESKEY} \
+        --shelley-vrf-key ${VRFKEY} \
+        --shelley-operational-certificate ${NODECERT}    
