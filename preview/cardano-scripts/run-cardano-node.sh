a41ab1cf653eb94c2264008b30b3c502d68d4322
solidsnakedev <jona.ca.eng@gmail.com>
2023-09-30 17:47:30 -0400

    conditional cardano-node run

--- preview/cardano-scripts/run-cardano-node.sh
+++ preview/cardano-scripts/run-cardano-node.sh
@@ -9,14 +9,24 @@ SOCKETPATH="/node/ipc/node.socket"
 KESKEY="/node/pool-keys/kes.skey"
 VRFKEY="/node/pool-keys/vrf.skey"
 NODECERT="/node/pool-keys/node.cert"
-    
-/usr/local/bin/cardano-node run \
-        --topology ${TOPOLOGY} \
-        --database-path ${DBPATH} \
-        --socket-path ${SOCKETPATH} \
-        --host-addr ${HOSTADDR} \
-        --port ${PORT} \
-        --config ${CONFIG} \
-        --shelley-kes-key ${KESKEY} \
-        --shelley-vrf-key ${VRFKEY} \
-        --shelley-operational-certificate ${NODECERT}    
+
+if [ -n KESKEY ] && [ -n VRFKEY ] && -n [NODECERT]; then
+        /usr/local/bin/cardano-node run \
+                --topology ${TOPOLOGY} \
+                --database-path ${DBPATH} \
+                --socket-path ${SOCKETPATH} \
+                --host-addr ${HOSTADDR} \
+                --port ${PORT} \
+                --config ${CONFIG} \
+                --shelley-kes-key ${KESKEY} \
+                --shelley-vrf-key ${VRFKEY} \
+                --shelley-operational-certificate ${NODECERT}
+else
+        /usr/local/bin/cardano-node run \
+                --topology ${TOPOLOGY} \
+                --database-path ${DBPATH} \
+                --socket-path ${SOCKETPATH} \
+                --host-addr ${HOSTADDR} \
+                --port ${PORT} \
+                --config ${CONFIG}\ 
+fi
