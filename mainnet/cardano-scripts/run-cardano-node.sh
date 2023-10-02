0db1aab244dabe21b9d5bdb706f8743714ac3b94
solidsnakedev <jona.ca.eng@gmail.com>
2023-10-02 12:59:51 -0400

    add conditional run script to preprod and mainnet

--- mainnet/cardano-scripts/run-cardano-node.sh
+++ mainnet/cardano-scripts/run-cardano-node.sh
@@ -9,14 +9,26 @@ SOCKETPATH="/node/ipc/node.socket"
 KESKEY="/node/pool-keys/kes.skey"
 VRFKEY="/node/pool-keys/vrf.skey"
 NODECERT="/node/pool-keys/node.cert"
-    
-/usr/local/bin/cardano-node run \
-        --topology ${TOPOLOGY} \
-        --database-path ${DBPATH} \
-        --socket-path ${SOCKETPATH} \
-        --host-addr ${HOSTADDR} \
-        --port ${PORT} \
-        --config ${CONFIG} \
-        --shelley-kes-key ${KESKEY} \
-        --shelley-vrf-key ${VRFKEY} \
-        --shelley-operational-certificate ${NODECERT}    
+
+if [ -f ${KESKEY} ] && [ -f ${VRFKEY} ] && [ -f ${NODECERT} ]; then
+        echo -e "RUNNING with \n ${KESKEY} \n ${VRFKEY} \n ${NODECERT}"
+        /usr/local/bin/cardano-node run \
+                --topology ${TOPOLOGY} \
+                --database-path ${DBPATH} \
+                --socket-path ${SOCKETPATH} \
+                --host-addr ${HOSTADDR} \
+                --port ${PORT} \
+                --config ${CONFIG} \
+                --shelley-kes-key ${KESKEY} \
+                --shelley-vrf-key ${VRFKEY} \
+                --shelley-operational-certificate ${NODECERT}
+else
+        echo "RUNNING STAND-ALONE"
+        /usr/local/bin/cardano-node run \
+                --topology ${TOPOLOGY} \
+                --database-path ${DBPATH} \
+                --socket-path ${SOCKETPATH} \
+                --host-addr ${HOSTADDR} \
+                --port ${PORT} \
+                --config ${CONFIG}
+fi
--- preprod/cardano-scripts/run-cardano-node.sh
+++ preprod/cardano-scripts/run-cardano-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HOSTADDR="0.0.0.0"
-PORT="6000"
+PORT="6001"
 TOPOLOGY="/node/configuration/topology.json"
 CONFIG="/node/configuration/config.json"
 DBPATH="/node/db"
@@ -9,14 +9,26 @@ SOCKETPATH="/node/ipc/node.socket"
 KESKEY="/node/pool-keys/kes.skey"
 VRFKEY="/node/pool-keys/vrf.skey"
 NODECERT="/node/pool-keys/node.cert"
-    
-/usr/local/bin/cardano-node run \
-        --topology ${TOPOLOGY} \
-        --database-path ${DBPATH} \
-        --socket-path ${SOCKETPATH} \
-        --host-addr ${HOSTADDR} \
-        --port ${PORT} \
-        --config ${CONFIG} \
-        --shelley-kes-key ${KESKEY} \
-        --shelley-vrf-key ${VRFKEY} \
-        --shelley-operational-certificate ${NODECERT}    
+
+if [ -f ${KESKEY} ] && [ -f ${VRFKEY} ] && [ -f ${NODECERT} ]; then
+        echo -e "RUNNING with \n ${KESKEY} \n ${VRFKEY} \n ${NODECERT}"
+        /usr/local/bin/cardano-node run \
+                --topology ${TOPOLOGY} \
+                --database-path ${DBPATH} \
+                --socket-path ${SOCKETPATH} \
+                --host-addr ${HOSTADDR} \
+                --port ${PORT} \
+                --config ${CONFIG} \
+                --shelley-kes-key ${KESKEY} \
+                --shelley-vrf-key ${VRFKEY} \
+                --shelley-operational-certificate ${NODECERT}
+else
+        echo "RUNNING STAND-ALONE"
+        /usr/local/bin/cardano-node run \
+                --topology ${TOPOLOGY} \
+                --database-path ${DBPATH} \
+                --socket-path ${SOCKETPATH} \
+                --host-addr ${HOSTADDR} \
+                --port ${PORT} \
+                --config ${CONFIG}
+fi
